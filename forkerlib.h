fe4862af1a707e47394af402d892e417597b72e2
jamyspencer <jamy_spencer@hotmail.com>
2017-03-27 16:27:27 -0500

    added header for LogStats in

--- forkerlib.h
+++ forkerlib.h
@@ -33,7 +33,7 @@ struct list* PushProcess(struct list* queue_head, struct list* process);
 struct list *MakeChild(struct list* head_ptr, pcb_t* my_pcb, int pcb_loc, struct timespec clock);
 void KillSlaves(struct list *hd_ptr, char* file_name);
 int SaveLog(char* log_file_name, pid_t pid, struct timespec clock, int queue, char* log_type);
-
+void LogStats(char* file_name, struct stats inf);
 struct list *returnTail(struct list *head_ptr);
 struct list *addNode(struct list *head_ptr, pid_t pid, int pcb_loc,struct timespec clock);
 struct list* destroyNode(struct list *head_ptr, pid_t pid, char* file_name);
